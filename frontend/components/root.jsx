5d2061733943cbb768831cfcae3064f1eea50e65
rsa2135 <ronariav@gmail.com>
2017-04-18 22:00:12 -0400

    End day 1

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,12 +3,30 @@ import { Provider } from 'react-redux';
 import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 
 import App from './app';
+import SessionFormContainer from './session_form/session_form_container'
 
 const Root = ({ store }) => {
+
+  const _ensureLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (!currentUser) {
+      replace('/login');
+    }
+  };
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  }
+
   return (
     <Provider store={ store }>
       <Router history={ hashHistory }>
         <Route path='/' components={ App }>
+          <Route path="/login" component={SessionFormContainer} onEnter={_redirectIfLoggedIn} />
+          <Route path="/signup" component={SessionFormContainer} onEnter={_redirectIfLoggedIn} />
         </Route>
       </Router>
     </Provider>
